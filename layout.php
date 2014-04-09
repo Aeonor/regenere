b283e9cc04d85435e28c484cfb8c18a42544ac0d
Aeonor <aeonor@gmail.com>
2014-04-09 16:51:29 +0200

    Styles

---
+++ layout.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1"/>
+        <meta name="application-name" content="Régénère"/>
+        <meta name="description" content="Régénère.org " />
+        <link rel="image_src" href="icon.png" />
+
+        <title>Régénère.com</title>
+        <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700|Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+        <link rel="stylesheet/less" type="text/css"  href="less/styles.less?<?php echo rand() ?>">
+        <link rel="icon" type="image/png" href="icon.png" />
+        <link rel="canonical" href="http://www.vivrecru.org"/>
+
+    </head>
+    <body class="<?php echo $_page ?>">
+
+        <div id="wrapper">
+            <div class="image">
+                
+                <?php include 'layout_header.php'; ?>
+                
+                <div id="container">
+                    <div class="container">
+                        <?php echo $_content; ?>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <footer>
+
+        </footer>
+
+
+        <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.7.0/less.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+        <script src="js/init.js"></script>
+    </body>
+</html>
---
+++ layout_header.php
@@ -0,0 +1,43 @@
+<header id="header">
+    <div class="container">
+        <div class="row">
+
+            <nav class="user">
+                <a href="#" class="#">Espace membre</a>
+            </nav>
+            <nav class="social pull">
+                <ul class="list-inline">
+                    <li><a href="#" class="fb">Facebook</a></li><li><a href="#" class="twitter">Twitter</a></li>
+                </ul>
+            </nav>
+        </div> 
+        <div class="row navbar-default">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+            <form method="get" class="pull-right col-xs-6 col-md-4" role="form">
+                <div class="input-group">
+                    <input type="search" name="q" placeholder="Rechercher sur le site" class="form-control" />
+                    <span class="input-group-btn"><button type="submit" name="search" value="Rechercher" class="btn btn-success"><i class="glyphicon glyphicon-search"></i></button></span>
+                </div>
+            </form>
+
+            <a href="#" id="logo" class="col-xs-4 col-md-3"><img src="img/regenere-logo.png" alt="Logo régénère" /></a>
+            <nav role="navigation" id="navbar">
+                <ul class="list-inline">
+                    <li><a href="#"><span>Vidéos & articles</span></a></li><li>
+                        <a href="#"><span>Livre d'or</span></a></li><li>
+                        <a href="#"><span>Outils régénération</span></a></li><li>
+                        <a href="#"><span>Boutique</span></a></li><li>
+                        <a href="#"><span>Stages & conférences</span></a></li><li>
+                        <a href="#"><span>Questions fréquentes</span></a></li><li class="asso">
+                        <a href="#"><span>L'association</span></a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
